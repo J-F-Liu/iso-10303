17e40a42a92cbe345d1af7e7becc8fd5353a4b7a
Junfeng Liu <china.liujunfeng@gmail.com>
2020-11-16 19:00:15 +0800

    Display time to read example file

--- parts/examples/read.rs
+++ parts/examples/read.rs
@@ -3,9 +3,10 @@ use iso_10303_parts::ap203;
 use iso_10303_parts::ap214;
 
 fn main() {
+    let instant = std::time::Instant::now();
     let mut reader = ap214::Ap214Reader::new();
-    // match reader.read("C:/Users/Liu/3D Objects/HandySCAN 3D_Demo part_CAD.stp") {
-    match reader.read("examples/ap214_example.stp") {
+    match reader.read("C:/Users/Liu/3D Objects/HandySCAN 3D_Demo part_CAD.stp") {
+        // match reader.read("examples/ap214_example.stp") {
         Ok(_) => {
             for context in reader.get_entities::<ap214::ApplicationContext>() {
                 println!("{:?}", context);
@@ -19,20 +20,20 @@ fn main() {
         }
         Err(err) => println!("{:?}", err),
     }
-
-    let mut reader = ap203::Ap203Reader::new();
-    match reader.read("examples/ap203_example.stp") {
-        Ok(_) => {
-            for context in reader.get_entities::<ap203::ApplicationContext>() {
-                println!("{:?}", context);
-            }
-            let mut total = 0;
-            for (type_id, entity_ids) in reader.type_ids {
-                println!("{:?} - {} ({})", type_id, reader.type_names[&type_id], entity_ids.len());
-                total += entity_ids.len();
-            }
-            println!("simple entities: {}", total);
-        }
-        Err(err) => println!("{:?}", err),
-    }
+    println!("elapsed time: {} seconds", instant.elapsed().as_secs_f64());
+    // let mut reader = ap203::Ap203Reader::new();
+    // match reader.read("examples/ap203_example.stp") {
+    //     Ok(_) => {
+    //         for context in reader.get_entities::<ap203::ApplicationContext>() {
+    //             println!("{:?}", context);
+    //         }
+    //         let mut total = 0;
+    //         for (type_id, entity_ids) in reader.type_ids {
+    //             println!("{:?} - {} ({})", type_id, reader.type_names[&type_id], entity_ids.len());
+    //             total += entity_ids.len();
+    //         }
+    //         println!("simple entities: {}", total);
+    //     }
+    //     Err(err) => println!("{:?}", err),
+    // }
 }
